7785bd9a87f9b3cc0ebed26b602cba6da078fdcf
Raghavendra Galeppa <Raghavendrar404@gmail.com>
2020-11-13 13:24:16 +0530

    First commit

---
+++ config.fish
@@ -0,0 +1,29 @@
+alias vfc="vim $config_fish"
+alias i="ipython3"
+alias sfc="source $config_fish"
+alias vrc="vim $config_vim"
+
+
+# Git aliases
+alias gs="git status"
+alias gc="git commit"
+alias gcm="git commit -m"
+alias ga="git add"
+alias gaa="git add ."
+
+alias gp="git push"
+alias gpom="git push origin master"
+alias gpo="git push origin"
+
+alias gl="git log --oneline"
+alias gd="git diff"
+
+# Temporary aliases
+alias cdd="cd ~/Desktop"
+alias cdr="cd ~/Desktop/RaghavendraGaleppa2020/"
+
+alias ph="cd $PROJECT_HOME"
+alias cdb="cd ~/Books"
+
+alias vea="source $VENV/venv/bin/activate.fish"
+alias ved="deactivate"
